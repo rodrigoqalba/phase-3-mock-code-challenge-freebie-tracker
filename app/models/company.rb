1ba0af1b08c21dae53619cf508a2e9069fbb3c69
Rodrigo Alba <rodrigo@megabadvisors.com>
2021-12-02 13:01:20 -0400

    not yet

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,8 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie(dev, item_name, value)
+        
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,21 @@
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meeseeks = Dev.create(name: "Mr. Meseeks")
+gazor = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "frisbee", value: 1, company_id: google.id, dev_id: rick.id)
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
